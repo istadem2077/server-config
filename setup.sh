9982b8d37000a7896c72826080c38062bf7fb4c3
Ismayil Taghi-Zada <46377455+istadem2077@users.noreply.github.com>
2023-12-20 11:40:50 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update
-apt install xorg xvfb python3-pip python3-selenium i3 kitty git
-apt install chromium-driver
+apt install xorg xvfb python3-pip python3-selenium i3 kitty git -y 
+apt install chromium-driver -y
 git clone https://github.com/istadem2077/CBTracker -b dec-ver /root/cb-may
 git clone https://github.com/istadem2077/CBTracker -b aug-ver /root/cb-mar
 mkdir -p /root/.config/i3/ && mv config /root/.config/i3/config
