0dc1ec3708035997cd4b081c8c57b8fe88cbd97e
Ismayil Taghi-Zada <46377455+istadem2077@users.noreply.github.com>
2023-12-20 11:45:07 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd /root/cb-mar && git pull
-cd /root/cb-dec && git pull
-python3 /root/cb-dec/tgsystem.py &
+#cd /root/cb-may && git pull
+python3 /root/cb-mar/tgsystem.py &
 while :
 do
 	#python3 /root/cb-nov/main.py &
