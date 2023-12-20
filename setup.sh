02f00cdcb37fd73bbe2bbe150e88b5951b758890
Ismayil Taghi-Zada <46377455+istadem2077@users.noreply.github.com>
2023-12-20 11:40:01 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,9 @@
 apt update
 apt install xorg xvfb python3-pip python3-selenium i3 kitty git
 apt install chromium-driver
-git clone https://github.com/istadem2077/CBTracker -b dec-ver cb-may
-git clone https://github.com/istadem2077/CBTracker -b aug-ver cb-mar
-mkdir -p .config/i3/ && mv config .config/i3/config
+git clone https://github.com/istadem2077/CBTracker -b dec-ver /root/cb-may
+git clone https://github.com/istadem2077/CBTracker -b aug-ver /root/cb-mar
+mkdir -p /root/.config/i3/ && mv config /root/.config/i3/config
 mkdir -p /etc/systemd/system/getty@tty1.service.d/ && mv override.conf /etc/systemd/system/getty@tty1.service.d/override.conf
 mv .bashrc /root/ && mv .profile /root/ && mv runner.sh /root/
+cp xorg.conf /etc/X11/
